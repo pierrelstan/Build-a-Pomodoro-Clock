0c891b68718a602393447b96191d77056df14430
@pierrelstan <stanleypl75@gmail.com>
2019-03-01 15:30:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    seconds: 25,
+    breakLength: 5,
+    sessionLength:25
+
+  }
+
+  tick() {
+    this.setState(state=> ({
+      seconds: state.seconds - 1
+    }))
+    if (this.state.seconds === -1) {
+      this.setState(state => ({
+        seconds: state.seconds + 26
+      }))
+    }
+  }
+
+ 
+ 
+componentDidMount(){
+  this.interval = setInterval(()=> this.tick(), 1000)
+
+
+
+}
+componentWillUnmount() {
+  clearInterval(this.interval)
+}
   render() {
+    const { seconds } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div><h1 style={{textAlign: "center"}}>Pomodoro Clock</h1></div>
+        <div style={{ textAlign: "center" }}>Seconds: {seconds}</div> 
+       <div>
+          <div id="break-decrement">down</div>
+          <div id="break-label">{this.state.breakLength}</div>
+          <div id="session-decrement">up</div>
+       </div>
+
+      <div>
+          <div id="break-decrement">down</div>
+          <div id="session-label">{this.state.sessionLength}</div>
+          <div id="session-decrement">up</div>
+      </div>
+       <div>
+          <div id="timer-label">Session</div>
+       </div>
+        
       </div>
     );
   }
