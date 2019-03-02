a10a0d51e33a2975c35714531524d42e72ac7309
@pierrelstan <stanleypl75@gmail.com>
2019-03-02 15:05:18 -0500

    Make the start and stop , reset buttons works

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-
- # Build-a-Pomodoro-Clock
+# Build-a-Pomodoro-Clock
 Build a Pomodoro Clock Freecodecamp
--- src/App.js
+++ src/App.js
@@ -2,58 +2,88 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
-  state={
-    seconds: 25,
-    breakLength: 5,
-    sessionLength:25
-
-  }
-
-  tick() {
-    
-    this.setState(state=> ({
-      seconds: state.seconds - 1
-    }))
-    if (this.state.seconds === -1) {
-      this.setState(state => ({
-        seconds: state.seconds + 26
-      }))
+  constructor(props){
+    super(props);
+    this.state = {
+      seconds: 25 * 60,
+      breakLength: 5,
+      sessionLength: 25
     }
   }
 
  
- 
-componentDidMount(){
-  this.interval = setInterval(()=> this.tick(), 1000)
-
-
-
+getMinutes(){
+ let min= Math.floor(("0" + this.state.seconds / 60))
+ return min
 }
-componentWillUnmount() {
+getSecondes(){
+  let  sec= ("0" + this.state.seconds % 60).slice(-2)
+  return sec
+}
+
+breakLength(){
+ if(this.getSecondes() === 57) {
+  this.setState({
+    seconds:clearInterval(this.interval)
+  })
+ }
+}
+  handleClickStart =()=> {
+
+    this.interval = setInterval(() => this.setState(state => ({
+      seconds: state.seconds - 1,
+    })), 1000)
+  }
+
+  handleClickReset=()=> {
+    this.setState({
+      seconds:this.state.sessionLength * 60
+    })
+  }
+ 
+ 
+// componentDidMount(){
+//   this.interval = setInterval(() => this.handleClickStart(), 1000)
+// }
+  handleClickStop=()=> {
   clearInterval(this.interval)
 }
   render() {
-    const { seconds } = this.state;
     return (
       <div>
         <div><h1 style={{textAlign: "center"}}>Pomodoro Clock</h1></div>
-        <div style={{ textAlign: "center" }}>
-          <div id="break-decrement">down</div>
-          <div id="break-label">{this.state.breakLength}</div>
-          <div id="session-decrement">up</div>
-       </div>
+        <div style={{ display: "flex",}}>
+          <div>
 
-        <div style={{ textAlign: "center" }}>
-          <div id="break-decrement">down</div>
-          <div id="session-label">{this.state.sessionLength}</div>
-          <div id="session-decrement">up</div>
-      </div>
+            <button id="break-decrement">up</button>
+            <div id="break-label">{this.state.breakLength}</div>
+            <button id="session-decrement">down</button>
+          </div>
+          <div>
+            <button id="break-decrement">up</button>
+            <div id="session-label">{this.state.sessionLength}</div>
+            <button id="session-decrement">down</button>
+          </div>
+        </div>
+     
         <div style={{ textAlign: "center" }}>
           <h1>Session</h1>
-          <div id="timer-label" >{seconds}</div>
+          <div id="timer-label" >{this.getMinutes()} : {this.getSecondes()} </div>
        </div>
-        
+        <div className="start_Stop_wrapper">
+         <div style={{display: "flex", justifyContent: "center"}}>
+           <div>
+              <button id="start_stop" onClick={this.handleClickStart}>Start</button>
+
+              <button onClick={this.handleClickStop}>Stop</button>
+              <button onClick={this.handleClickReset}>Reset</button>
+
+           </div>
+           
+         </div>
+        </div>
       </div>
     );
   }
