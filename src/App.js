cf82e285af9f39188f0efbff18235f61504f2191
@pierrelstan <stanleypl75@gmail.com>
2019-03-05 13:40:21 -0500

    Add the range in breakLength and sessionLength and add some css

--- src/App.js
+++ src/App.js
@@ -2,47 +2,120 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-
+const initialState = {
+  seconds: 25 * 60,
+  sessionLength: 25,
+  breakLength: 5
+}
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       seconds: 25 * 60,
       breakLength: 5,
-      sessionLength: 25
+      break: false,
+      sessionLength: 25,
+      session:false
     }
   }
 
+
+componentWillMount(){
+  clearInterval(this.sam)
+}
+
+  handleIncremetBreakLength =()=> {
+    this.setState((prevState) => ({
+      breakLength: this.state.breakLength + 1,
+    }))
+  }
  
+  handleDecremetBreakLength=()=> {
+    this.setState((prevState) => ({
+      breakLength: prevState.breakLength - 1,
+    }))
+    if (this.state.breakLength === 1) {
+      console.log("work")
+      this.setState(() => ({
+        breakLength: 1
+      }))
+    }
+    
+    else {
+      return 
+    }
+    // if (this.state.seconds === 55) {
+    //   let sam = setInterval(() => console.log("huhuhu"), 1000)
+    //   return sam
+    // }
+  }
+  handleIncremetsessionLength = () => {
+    this.setState((prevState) => ({
+      sessionLength: prevState.sessionLength + 1,
+      // break:false,
+      session:true
+    }))
+   
+    if(this.state.session === true){
+      this.setState(prevState => ({
+        seconds: prevState.sessionLength * 60
+      }))
+    }
+  }
+  handleDecremetsessionLength = () => {
+    this.setState((prevState) => ({
+      sessionLength: prevState.sessionLength - 1,
+      session: true,
+     
+    }))
+    if (this.state.session === true) {
+      this.setState(prevState => ({
+        seconds: prevState.sessionLength * 60
+      }))
+    }
+    if (this.state.sessionLength === 1) {
+      this.setState((state) => ({
+        sessionLength: 1
+      }))
+    }
+    if(this.state.sessionLength === 1){
+      this.setState((state)=> ({
+       seconds: 1 * 60
+      }))
+    }
+  }
+
 getMinutes(){
  let min= Math.floor(("0" + this.state.seconds / 60))
  return min
 }
 getSecondes(){
   let  sec= ("0" + this.state.seconds % 60).slice(-2)
-  return sec
+    return sec
 }
-
-breakLength(){
- if(this.getSecondes() === 57) {
-  this.setState({
-    seconds:clearInterval(this.interval)
-  })
- }
-}
+  
   handleClickStart =()=> {
 
     this.interval = setInterval(() => this.setState(state => ({
       seconds: state.seconds - 1,
     })), 1000)
-  }
+
+  this.setState({
+    break: true
+  })
+   if (this.state.seconds === 55) {
+      console.log("stats")
+    }
+    
+}
 
   handleClickReset=()=> {
-    this.setState({
-      seconds: this.state.sessionLength * 60
-    })
+    this.setState(()=>({
+      ...initialState
+    }))
     clearInterval(this.interval)
-  }
+
+}
  
   handleClickStop=()=> {
   clearInterval(this.interval)
@@ -50,36 +123,43 @@ breakLength(){
   render() {
     return (
       <div>
-        <div><h1 style={{textAlign: "center"}}>Pomodoro Clock</h1></div>
-        <div style={{display: "flex", justifyContent:"center"}}>
-          <div>
-             <h2>break Length</h2>
-            <div id="break-decrement"><i class="far fa-arrow-alt-circle-up"></i></div>
-            <div id="break-label">{this.state.breakLength}</div>
-            <div id="session-decrement"><i class="far fa-arrow-alt-circle-down"></i></div>
+       <h1 className="title-Pomodoro" style={{textAlign: "center"}}>Pomodoro Clock</h1>
+        <div className="container" >
+          
+     
+        <div className="buttom-label-wrapper">
+          <div style={{ display: "grid" }}>
+              <h2 id="break-label">break Length</h2>
+          <div style={{display:"flex", justifyContent:"center"}}>
+                <div id="break-decrement" onClick={this.handleIncremetBreakLength}><i className="far fa-arrow-alt-circle-up"></i></div>
+                <div id="break-length" >{this.state.breakLength}</div>
+                <div id="session-decrement" onClick={this.handleDecremetBreakLength}><i className="far fa-arrow-alt-circle-down"></i></div>
+       </div>
+       </div>
+
+          <div style={{display:"grid"}}>
+              <h2 id="session-label">session Length</h2>
+            <div style={{ display: "flex", justifyContent: "center"}}>
+                <div id="break-increment" onClick={this.handleIncremetsessionLength}><i className="far fa-arrow-alt-circle-up"></i></div>
+                <div  id="session-length" >{this.state.sessionLength}</div>
+                <div id="session-increment" onClick={this.handleDecremetsessionLength}><i className="far fa-arrow-alt-circle-down"></i></div>
+            </div>
           </div>
-          <div>
-            <h2>session Length</h2>
-            <div id="break-decrement"><i class="far fa-arrow-alt-circle-up"></i></div>
-            <div id="session-label">{this.state.sessionLength}</div>
-            <div id="session-decrement"><i class="far fa-arrow-alt-circle-down"></i></div>
-          </div>
+        </div>
         </div>
      <div className="circle-wrapper">
           <div id="circle">
-            <h2>Session</h2>
-            <div id="timer-label" >{this.getMinutes()} : {this.getSecondes()} </div>
+            <h2 id="timer-label">Session</h2>
+            <div  id="time-left">{this.getMinutes()} : {this.getSecondes()} </div>
           </div>
      </div>
      
         <div className="start_Stop_wrapper">
          <div className="StartAndStop-Wrapper">
            <div>
-              <button id="start_stop" onClick={this.handleClickStart}>Start</button>
-
-              <button onClick={this.handleClickStop}>Stop</button>
-              <button onClick={this.handleClickReset}>Reset</button>
-
+              <button id="start_stop" onClick={this.handleClickStart} className="button button2"><i className="fas fa-play"></i></button>
+              <button onClick={this.handleClickStop} className="button button2"><i className="fas fa-stop"></i></button>
+              <button onClick={this.handleClickReset} className="button button2" id="reset"><i className="fas fa-sync-alt"></i></button>
            </div>
            
          </div>
