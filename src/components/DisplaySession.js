841bd5b3d26ba66463e33a605c9fd591fb3c9583
@pierrelstan <stanleypl75@gmail.com>
2019-03-11 21:42:19 -0400

    Add leftValeue and titleTimer

--- src/components/DisplaySession.js
+++ src/components/DisplaySession.js
@@ -1,14 +1,23 @@
 
 import React from "react";
 
+const leftValue=(val)=> {
+if(val <= 9 ){ 
+    return `0${val}`;
+}
+else {
+    return `${ val }`
+}
+
+}
 class DisplaySession extends React.Component {
     render(){
-        const { getMinutes , getSecondes} = this.props;
+        const { getMinutes, getSecondes, titleTimer} = this.props;
         return (
             <div className="circle-wrapper">
                 <div id="circle">
-                    <h2 id="timer-label">Session</h2>
-                    <div id="time-left">{getMinutes} : {getSecondes} </div>
+                    <h2 id="timer-label">{titleTimer}</h2>
+                    <div id="time-left">{leftValue(getMinutes)} : {getSecondes}</div>
                 </div>
             </div>
         )
