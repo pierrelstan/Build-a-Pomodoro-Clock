cea8074ff2926d6edc042fd6fd49120b42c12f01
@pierrelstan <stanleypl75@gmail.com>
2019-03-15 08:52:30 -0400

    Add to comment the Button with the onclick handleClickStop

--- src/components/ButtonStartPauseReset.js
+++ src/components/ButtonStartPauseReset.js
@@ -19,14 +19,14 @@ play(){
             <div className="start_Stop_wrapper">
                 <div className="StartAndStop-Wrapper">
                     <div>
-                        <button id="start_stop" onClick={handleClickStart} className="button button2 "><i className="fas fa-play"></i>
+                        <button onClick={handleClickStart} className="button button2 " id="start_stop"><i className="fas fa-play"></i>
                             <audio
                                 id="beep" src={`https://res.cloudinary.com/dteuk7cbl/video/upload/v1546541304/Audio/CardiakClap.mp3`}
                                 ref={ref => this.audio = ref}
                             >
                             </audio>
                         </button>
-                        <button onClick={handleClickStop} className="button button2"><i className="fas fa-stop"></i></button>
+                        {/* <button onClick={handleClickStop} className="button button2" ><i className="fas fa-stop"></i></button> */}
                         <button onClick={handleClickReset} className="button button2" id="reset"><i className="fas fa-sync-alt"></i></button>
                     </div>
 
