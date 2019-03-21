35eb473a779d1e70b4e99bd4a20a17e4b5fca422
@pierrelstan <stanleypl75@gmail.com>
2019-03-21 12:26:33 -0400

    Add break.js

---
+++ src/components/break.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Break =()=> (
+    <div>
+        <button id="break-decrement">down</button>
+        <div id="break-label">{this.state.breakLength}</div>
+        <button d="break-increment">up</button>
+    </div>
+)
+export default Break;
