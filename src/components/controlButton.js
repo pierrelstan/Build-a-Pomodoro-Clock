982ced8dca3b577eb95d5e85b3214c2fb18a0b7a
@pierrelstan <stanleypl75@gmail.com>
2019-03-21 12:49:48 -0400

    Update controlButton.js

--- src/components/controlButton.js
+++ src/components/controlButton.js
@@ -1,10 +1,10 @@
 import React from "react";
-const ControlButton =()=> (
+const ControlButton =({handleClickStart, handleClickReset})=> (
     <div className="start_Stop_wrapper">
         <div style={{ display: "flex", justifyContent: "center" }}>
             <div>
-                <button id="start_stop" onClick={this.handleClickStart}>Start</button>
-                <button onClick={this.handleClickReset}>Reset</button>
+                <button id="start_stop" onClick={handleClickStart}>Start</button>
+                <button onClick={handleClickReset}>Reset</button>
 
             </div>
 
