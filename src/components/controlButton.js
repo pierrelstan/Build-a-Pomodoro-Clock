d3620173af0d6dc6f490c1eff91244f493bfe128
Stanley <stanleypl75@gmail.com>
2019-03-29 23:43:49 -0400

    Update ControlButton.js

--- src/components/controlButton.js
+++ src/components/controlButton.js
@@ -1,14 +1,11 @@
 import React from "react";
-const ControlButton =({handleClickStart, handleClickReset})=> (
-    <div className="start_Stop_wrapper">
-        <div style={{ display: "flex", justifyContent: "center" }}>
-            <div>
-                <button id="start_stop" onClick={handleClickStart}>Start</button>
-                <button onClick={handleClickReset} id="reset">Reset</button>
+const ControlButton =({handleClickStart, handleClickReset,start_stop})=> (
+           <div style={{display:"flex",justifyContent:"center"}}>
+               <div  className="buttom-label-wrapper">
+                <button id="start_stop" onClick={handleClickStart} className="button button2">{start_stop ? <i class="fas fa-play"></i> : <i class="fas fa-stop"></i>}</button>
+                <button onClick={handleClickReset} id="reset" className="buttonreset button2"><i class="fas fa-sync-alt"></i></button>
 
             </div>
-
-        </div>
-    </div>
+            </div>
 )
 export default ControlButton;
