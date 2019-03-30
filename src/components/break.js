eaa392c7cd7456b99d814f53b80aca0aad3101c9
Stanley <stanleypl75@gmail.com>
2019-03-29 23:42:26 -0400

    Update break.js

--- src/components/break.js
+++ src/components/break.js
@@ -1,10 +1,12 @@
 import React from "react";
 const Break = ({ breakLength, handleClickBreakLengthDown, handleClickBreakLengthUp })=> (
-    <div>
-        <h1 id="break-label">Break Length</h1>
-        <button id="break-increment" onClick={handleClickBreakLengthUp}>up</button>
-        <div id="break-length">{ breakLength }</div>
-        <button  id="break-decrement" onClick={handleClickBreakLengthDown}>down</button>
+    <div className="container-break">
+        <h3 id="break-label">Break</h3>
+        <div style={{display:"flex", justifyContent:"center"}}>
+        <div id="break-increment" onClick={handleClickBreakLengthUp}><i class="far fa-arrow-alt-circle-up"></i></div>
+        <div id="break-length" style={{display:"flex", justifyContent:"center", alignItems:"center"}}>{ breakLength }</div>
+        <div  id="break-decrement" onClick={handleClickBreakLengthDown}><i class="far fa-arrow-alt-circle-down"></i></div>
+        </div>
     </div>
 )
 
